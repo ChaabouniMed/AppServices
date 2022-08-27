280335bfd627dbb9ff0cd5aa68f193c9b8b1562b
unknown <mahdi.ayadi@ensi-uma.tn>
2022-08-27 22:33:29 +0100

    temporary db

--- client/src/firebase-config.jsx
+++ client/src/firebase-config.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAy6STnu2SaKTZvjPoyVESq5ubrCTa7QFA",
-    authDomain: "reactapp-800a9.firebaseapp.com",
-    projectId: "reactapp-800a9",
-    storageBucket: "reactapp-800a9.appspot.com",
-    messagingSenderId: "249367253558",
-    appId: "1:249367253558:web:f1abc83e3b7008b2bb3e97",
-    measurementId: "G-EWJ5570CKP"
+  apiKey: "AIzaSyCjtN560XDoO9g_Fft-rO2tokNO0z7oRJ0",
+  authDomain: "myapp-57f8f.firebaseapp.com",
+  projectId: "myapp-57f8f",
+  storageBucket: "myapp-57f8f.appspot.com",
+  messagingSenderId: "969742223179",
+  appId: "1:969742223179:web:048939332e8ec206260ace",
+  measurementId: "G-CT29VX5JMT"
   };
 
   const app =initializeApp(firebaseConfig)
