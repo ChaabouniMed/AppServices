c2210894f1ecb600b3443b7c52bc7dc6ded69575
Mahdi Ayadi <mahdi.ayadi@ensi-uma.tn>
2022-09-15 14:18:53 +0100

    database

--- client/src/firebase-config.jsx
+++ client/src/firebase-config.jsx
@@ -4,15 +4,14 @@ import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjtN560XDoO9g_Fft-rO2tokNO0z7oRJ0",
-  authDomain: "myapp-57f8f.firebaseapp.com",
-  projectId: "myapp-57f8f",
-  storageBucket: "myapp-57f8f.appspot.com",
-  messagingSenderId: "969742223179",
-  appId: "1:969742223179:web:048939332e8ec206260ace",
-  measurementId: "G-CT29VX5JMT"
-  };
-
+apiKey: "AIzaSyD0GzTjdSm5o-TwPNisbmtKECS4MLYxVsw",
+  authDomain: "project2-9cb6a.firebaseapp.com",
+  projectId: "project2-9cb6a",
+  storageBucket: "project2-9cb6a.appspot.com",
+  messagingSenderId: "170336948566",
+  appId: "1:170336948566:web:30cb5f3f170a243b6ec6f7",
+  measurementId: "G-FFP4JXWK4K"
+}
   const app =initializeApp(firebaseConfig)
   export const auth = getAuth(app)
   export const db = getFirestore(app);
